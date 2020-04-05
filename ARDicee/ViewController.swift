8c2674c159b39bab69a593c593d39302b9ec723a
fritzgt <tuanis@live.com>
2020-04-05 16:46:39 +0300

    initial commit

---
+++ ARDicee/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by FGT MAC on 4/5/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        //Create a sphere
+        let sphere = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        
+        //Material downloaded from solarsystemscope.com
+        material.diffuse.contents = UIImage(named: "art.scnassets/2k_mercury.jpg")
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(0, 0.1, -0.5)
+        
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        //Add lighting for true 3d effects/shodows
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+
+}
